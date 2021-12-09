e75f95994769c3d60b8da446e1e1d91573025b70
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-12-09 15:39:37 +0000

    created canvas, created borders

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// create canvas
+
+const { World, Engine, Render, Runner, Bodies} = Matter;
+
+// Create an Engine that automatically produces a world
+const engine = Engine.create();
+
+// engine.world.gravity.y = 0
+
+const {world} = engine;
+
+//Define size of rendered World
+const height= window.innerHeight*0.8;
+const width= window.innerHeight*0.8;
+
+//Create a render to show the world
+const renderTris = Render.create({
+  element: document.querySelector("#tela"),  //where to show the world
+  engine: engine, //use this engine
+  options:{
+    wireframes: true,
+    width, //dimensions in px of the world
+    height
+  }
+}); 
+
+//call render to visualise a so far empty world
+Render.run(renderTris) //run the render object created above
+//Render needs a runner to connect world and engine
+Runner.run(Runner.create(), engine);
+
+const circle = Bodies.circle( width/2, height/2, height/5,
+  {
+    // label: "cerchio",
+    // isStatic: true,
+  }
+)
+
+World.add(world, circle)
+
+//create borders
+
+const borders =[
+
+  Bodies.rectangle(width/2, 0, width, 20, {
+    isStatic: true
+  }),
+
+  Bodies.rectangle(width/2, height, width, 20, {
+    isStatic: true
+  }),
+
+  Bodies.rectangle(0, height/2, 20, height, {
+    isStatic: true
+  }),
+
+  Bodies.rectangle(width, height/2, 20, height, {
+    isStatic: true
+  })
+
+]
+
+World.add(world, borders)
+
+
+//create grid
